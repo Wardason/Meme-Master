1a69e14efb0e00891c88be59f7591071f3eff370
Leonard <66140015+Wardason@users.noreply.github.com>
2021-01-29 15:43:56 +0100

    Added all files

---
+++ Cogs/facts.py
@@ -0,0 +1,43 @@
+from discord.ext import commands
+from aiohttp import request
+import discord
+
+
+class Fact(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(brief="Returns your gay rate in percent")
+    async def fact(self, ctx, animal: str):
+        if (animal := animal.lower()) in ("dog", "cat", "panda", "fox", "bird", "koala"):
+
+            fact_url = f"https://some-random-api.ml/facts/{animal}"
+            image_url = f"https://some-random-api.ml/img/{'birb' if animal == 'bird' else animal}"
+
+            async with request('GET', image_url, headers={}) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    image_link = data["link"]
+                else:
+                    image_link = None
+
+            async with request('GET', fact_url, headers={}) as response:
+                if response.status == 200:
+                    data = await response.json()
+
+                    embed = discord.Embed(title=f"{animal.title()} fact",
+                                          description=data["fact"],
+                                          color=discord.Color.dark_purple())
+                    if image_link is not None:
+                        embed.set_image(url=image_link)
+
+                    await ctx.send(embed=embed)
+
+                else:
+                    await ctx.send(f"API returned a {response.status} status.")
+        else:
+            await ctx.send("No facts are available for that animal")
+
+
+def setup(bot):
+    bot.add_cog(Fact(bot))
--- main.py
+++ main.py
@@ -16,4 +16,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f'Cogs.{filename[:-3]}')
 
-bot.run('NzgyMjc0OTU5ODQ5Njg0OTkz.X8J0VA.sZrIlg_bs0X9emcDpWkQKgCn2Uk')
+bot.run('')
