e6681f6de1dd2cbebd428eafea652b3def3894b1
Leonard <66140015+Wardason@users.noreply.github.com>
2021-01-29 17:46:38 +0100

    Securing the Token

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from discord.ext import commands
-import os
 import discord
+import json
+import os
+
+if os.path.exists(os.getcwd() + "/config.json"):
+    with open("./config.json") as f:
+        configData = json.load(f)
+else:
+    configTemplate = {"Token": "", "Prefix": "$"}
+
+    with open(os.getcwd() + "/config.json", "w+") as f:
+        json.dump(configTemplate, f)
+
+token = configData["Token"]
+prefix = configData["Prefix"]
 
 
-bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+
+bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 bot.remove_command('help')
 
 
@@ -16,4 +30,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f'Cogs.{filename[:-3]}')
 
-bot.run('')
+bot.run(token)
